d7bade74860c581882328f79d31d6a7094fb1412
ATomilov <tomilovlehach@mail.ru>
2017-01-30 23:40:53 +0600

    Edit the images' path

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ jQuery(function ($) {
         $('#profile-click').click(function () {
            $(this).addClass('tab-active');
            if ( $(this).hasClass('tab-active') ) {
-               $('#profile-tab-img img').attr('src', './img/profile/profile.png');
+               $('#profile-tab-img img').attr('src', './img/profile.png');
                $('#work-click').removeClass('tab-active');
-               $('#work-tab-img img').attr('src', './img/profile/work_gray.png');
+               $('#work-tab-img img').attr('src', './img/work_gray.png');
                $('#resume-click').removeClass('tab-active');
-               $('#resume-tab-img img').attr('src', './img/profile/resume_gray.png');
+               $('#resume-tab-img img').attr('src', './img/resume_gray.png');
                $('#blog-click').removeClass('tab-active');
-               $('#blog-tab-img img').attr('src', './img/profile/blog_gray.png');
+               $('#blog-tab-img img').attr('src', './img/blog_gray.png');
                $('#contact-click').removeClass('tab-active');
-               $('#contact-tab-img img').attr('src', './img/profile/contact_gray.png');
+               $('#contact-tab-img img').attr('src', './img/contact_gray.png');
            }
            else {
                $('#profile-tab-img img').attr('src', './img/profile/profile_gray.png');
@@ -21,15 +21,15 @@ jQuery(function ($) {
         $('#work-click').click(function () {
             $(this).addClass('tab-active');
             if ( $(this).hasClass('tab-active') ) {
-                $('#work-tab-img img').attr('src', './img/profile/work.png');
+                $('#work-tab-img img').attr('src', './img/work.png');
                 $('#profile-click').removeClass('tab-active');
-                $('#profile-tab-img img').attr('src', './img/profile/profile_gray.png');
+                $('#profile-tab-img img').attr('src', './img/profile_gray.png');
                 $('#resume-click').removeClass('tab-active');
-                $('#resume-tab-img img').attr('src', './img/profile/resume_gray.png');
+                $('#resume-tab-img img').attr('src', './img/resume_gray.png');
                 $('#blog-click').removeClass('tab-active');
-                $('#blog-tab-img img').attr('src', './img/profile/blog_gray.png');
+                $('#blog-tab-img img').attr('src', './img/blog_gray.png');
                 $('#contact-click').removeClass('tab-active');
-                $('#contact-tab-img img').attr('src', './img/profile/contact_gray.png');
+                $('#contact-tab-img img').attr('src', './img/contact_gray.png');
             }
             else {
                 $('#work-tab-img img').attr('src', './img/profile/work_gray.png');
@@ -38,52 +38,52 @@ jQuery(function ($) {
         $('#resume-click').click(function () {
             $(this).addClass('tab-active');
             if ( $(this).hasClass('tab-active') ) {
-                $('#resume-tab-img img').attr('src', './img/profile/resume.png');
+                $('#resume-tab-img img').attr('src', './img/resume.png');
                 $('#work-click').removeClass('tab-active');
-                $('#work-tab-img img').attr('src', './img/profile/work_gray.png');
+                $('#work-tab-img img').attr('src', './img/work_gray.png');
                 $('#profile-click').removeClass('tab-active');
-                $('#profile-tab-img img').attr('src', './img/profile/profile_gray.png');
+                $('#profile-tab-img img').attr('src', './img/profile_gray.png');
                 $('#blog-click').removeClass('tab-active');
-                $('#blog-tab-img img').attr('src', './img/profile/blog_gray.png');
+                $('#blog-tab-img img').attr('src', './img/blog_gray.png');
                 $('#contact-click').removeClass('tab-active');
-                $('#contact-tab-img img').attr('src', './img/profile/contact_gray.png');
+                $('#contact-tab-img img').attr('src', './img/contact_gray.png');
             }
             else {
-                $('#resume-tab-img img').attr('src', './img/profile/resume_gray.png');
+                $('#resume-tab-img img').attr('src', './img/resume_gray.png');
             }
         });
         $('#blog-click').click(function () {
             $(this).addClass('tab-active');
             if ( $(this).hasClass('tab-active') ) {
-                $('#blog-tab-img img').attr('src', './img/profile/blog.png');
+                $('#blog-tab-img img').attr('src', './img/blog.png');
                 $('#work-click').removeClass('tab-active');
-                $('#work-tab-img img').attr('src', './img/profile/work_gray.png');
+                $('#work-tab-img img').attr('src', './img/work_gray.png');
                 $('#profile-click').removeClass('tab-active');
-                $('#profile-tab-img img').attr('src', './img/profile/profile_gray.png');
+                $('#profile-tab-img img').attr('src', './img/profile_gray.png');
                 $('#resume-click').removeClass('tab-active');
-                $('#resume-tab-img img').attr('src', './img/profile/resume_gray.png');
+                $('#resume-tab-img img').attr('src', './img/resume_gray.png');
                 $('#contact-click').removeClass('tab-active');
-                $('#contact-tab-img img').attr('src', './img/profile/contact_gray.png');
+                $('#contact-tab-img img').attr('src', './img/contact_gray.png');
             }
             else {
-                $('#blog-tab-img img').attr('src', './img/profile/blog_gray.png');
+                $('#blog-tab-img img').attr('src', './img/blog_gray.png');
             }
         });
         $('#contact-click').click(function () {
             $(this).addClass('tab-active');
             if ( $(this).hasClass('tab-active') ) {
-                $('#contact-tab-img img').attr('src', './img/profile/contact.png');
+                $('#contact-tab-img img').attr('src', './img/contact.png');
                 $('#work-click').removeClass('tab-active');
-                $('#work-tab-img img').attr('src', './img/profile/work_gray.png');
+                $('#work-tab-img img').attr('src', './img/work_gray.png');
                 $('#profile-click').removeClass('tab-active');
-                $('#profile-tab-img img').attr('src', './img/profile/profile_gray.png');
+                $('#profile-tab-img img').attr('src', './img/profile_gray.png');
                 $('#resume-click').removeClass('tab-active');
-                $('#resume-tab-img img').attr('src', './img/profile/resume_gray.png');
+                $('#resume-tab-img img').attr('src', './img/resume_gray.png');
                 $('#blog-click').removeClass('tab-active');
-                $('#blog-tab-img img').attr('src', './img/profile/blog_gray.png');
+                $('#blog-tab-img img').attr('src', './img/blog_gray.png');
             }
             else {
-                $('#contact-tab-img img').attr('src', './img/profile/contact_gray.png');
+                $('#contact-tab-img img').attr('src', './img/contact_gray.png');
             }
         });
     });
